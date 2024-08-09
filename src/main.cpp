c4b5739912cabd473604516d07dc63ceccb64470
caio-o <caiusbarbieri@gmail.com>
2024-08-09 18:21:30 -0300

    Ad. pasta src/ e divid. código em teste.cpp e teste.hpp

---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+/* Arquivo main.cpp por enquanto sem conteudo. */
+
+#include "testes.hpp"
+
+int main()
+{
+    testeJanela();
+
+    return 0;
+}
---
+++ src/testes.hpp
@@ -0,0 +1,10 @@
+/**
+ *    Arquivo que define funcoes para testar as funcionalidades
+ *  da SFML e as nossas proprias classes e funcoes.
+ */
+
+/**
+ * AUTOR:  Equipe do SFML
+ * FUNCAO: Cria uma janela com um circulo, e espera que o usuario a feche.
+ */
+void testeJanela();
