8a869a6dfc0325065ad08499ea184e2cf720fb06
caio-o <caiusbarbieri@gmail.com>
2024-08-10 11:23:02 -0300

    Criar Erros.hpp (def. mensagens de erro).

---
+++ src/Erros.hpp
@@ -0,0 +1,16 @@
+/**
+ *   AUTOR  : Caio e Silva Barbieri (caio-o) (ateh agora).
+ *   DATA   : 09/08/2024 19:38
+ *   Arquivo com definicoes de mensagens-padrao para cada tipo 
+ *   de erro de execucao.
+ */
+
+
+#ifndef _ERROS_HPP
+#define _ERROS_HPP
+
+    #define ERRO_DELETE_NULLPTR "Nao se pode deletar ponteiro NULL."
+    #define ERRO_INCLUI_NULLPTR "Ponteiro NULL nao incluido na lista."
+
+
+#endif
